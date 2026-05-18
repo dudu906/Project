edd52fbd2da28b6e713da46d562d0e32a5a25dfc
Eduardo <seuemail@exemplo.com>
2026-05-17 22:56:11 -0300

    add. readme.md

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,15 +1,15 @@
 function toggleMode() {
-    const html = document.documentElement;
-    html.classList.toggle('light')
- 
-    const img= document.querySelector("#profile img")
-    if(html.classList.contains('light')){
-  img.setAttribute('src', 'assets/avatar-light.png')
+const html = document.documentElement
+html.classList.toggle('light')
 
-  } 
-  else {
-  img.setAttribute('src', 'assets/avatar.png')
+const img = document.querySelector('#profile img')
 
-  } 
- 
+
+if (html.classList.contains('light')) {
+  img.setAttribute('src', './assets/Avatar.light.png')
+}
+
+else {
+img.setAttribute('src', './assets/Avatar.dark.png')
+}
 }
